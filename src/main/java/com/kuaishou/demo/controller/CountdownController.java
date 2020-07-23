262b5bfbb8990dba5243d3e145035ca349ec5b35
shishi <1107474494@qq.com>
2020-07-23 16:39:34 +0800

    新增组件

--- src/main/java/com/kuaishou/demo/controller/CountdownController.java
+++ src/main/java/com/kuaishou/demo/controller/CountdownController.java
@@ -5,23 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import static com.kuaishou.demo.common.HtmlCommon.getDuMSG;
-
 /**
  * <pre>
  * Description
  * @author shishi
- * 2020/5/15 17:52
+ * 2020/7/23 16:19
  * </pre>
  */
 @Controller
-public class Du {
-
-    @GetMapping("du")
+public class CountdownController {
+    @GetMapping("countdown")
     @ResponseBody
-    public ModelAndView du() throws Exception {
-        return new ModelAndView("du", "_du", getDuMSG());
+    public ModelAndView countdown() throws Exception {
+        return new ModelAndView("countdown");
     }
-
 
 }
